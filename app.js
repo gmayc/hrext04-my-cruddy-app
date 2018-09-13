b849dcc3ed91615e30c53b397709f22e5dfde4b9
Guillermo Maycotte <gmaycotte93@gmail.com>
2018-09-13 17:38:44 -0500

    Table and cells display correctly. Songs get deleted from list. Table clears anytime song is added or deleted

--- app.js
+++ app.js
@@ -1,77 +1,88 @@
 $(document).ready(function() {
 
+  var songsStored = Object.keys(localStorage);
+// checking if local stoarge is empty, else append keys to song list
+  if (songsStored.length !== 0){
 
+    for(let i = 0; i < songsStored.length; i++){
+      // console.log(songsStored[i]);
+      var songInList = `<option class='stored-song' id=${songsStored[i]}>${songsStored[i]}</option>`;
+      $('#songs-list').append(songInList); 
+    }
+  }
   $(".add-data-btn").on("click", function(){
 
-    // store values
+    let songTitle = $(".song-title").val();
+    var addedSong =`<option class='stored-song' id='${songTitle}'>${songTitle}</option>`;
     let artist = $(".artist").val();
     let album = $(".album").val();
-    let songTitle = $(".song-title").val();
     let BPM = $(".B-P-M").val();
     let numOfBars = Number($(".number-of-bars").val());
     let instruments = $(".instruments").val();
     let instArray = instruments.split(' ');
-    console.log(instArray.length);
+    var songStrObj = JSON.stringify({artist: artist, album: album, song: songTitle, BPM: BPM, Bars: numOfBars, instruments: instruments});
+    // console.log(instArray.length);
+
+    // store values
+    localStorage.setItem(songTitle,songStrObj);
+
+    // table generator
+    tableGenerator(numOfBars,instArray);
+
     // clear values
+    $('.song-selector').val('');
+    $(".song-title").val("");
     $(".artist").val("");
     $(".album").val("");
+    $(".B-P-M").val("");
+    $(".number-of-bars").val("");
+    $(".instruments").val("");
 
-    console.log(typeof numOfBars);
-    localStorage.setItem(songTitle,JSON.stringify({artist: artist, album: album, song: songTitle, BPM: BPM, Bars: numOfBars, instruments: instruments}));
-    // console.log(localStorage);
-    $(".stored-songs").append(`<option>${songTitle}</option>`);
+    // console.log(numOfBars);
     
-    // let bar = `<td class="bar"> ${} </td>`;
-    for (let i = 0; i <= numOfBars; i++) {
-      // $(".t1").html(`<tr class="bar"> ${i} </tr>`);
-      $('#t1').append(`<th class="bar">`+ i +`</th>`)
-      
+
+    $("#songs-list").append(addedSong);
+
+
+  }); // add btn ends
+
+  $('.song-selector').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+
+        var selectedSong =$('.song-selector').val();
+        
+        if (localStorage.hasOwnProperty(selectedSong)) {
+
+          tableGenerator(JSON.parse(localStorage[selectedSong]).Bars,(JSON.parse(localStorage[selectedSong]).instruments).split(' '));
+        }
     }
-    if (instArray[0] === "") {
-      alert('John Cage\'s 4:33 is not accepted. Please input an instrument...')
-    } else {
-      for (let i = 0; i < instArray.length; i++) {
-        var value1 = Math.floor(Math.random() * Math.floor(255))
-        var value2 = Math.floor(Math.random() * Math.floor(255))
-        var value3 = Math.floor(Math.random() * Math.floor(255))
-        $('#t1').append(`<tr><td class="${instArray[i]}">${instArray[i]}</td>`);
-        $(`.${instArray[i]}`).css('background-color', `rgb(${value1}, ${value2}, ${value3})`);
-      }
+});
+ 
 
-    }
-    //console.log(localStorage);
-    // how can we delegate this event to the outer html node?
-    // https://learn.jquery.com/events/event-delegation/
+  $(".del-data-btn").on("click", function() {
+    alert('item deleted? check the console'); // maybe change to a window.confirm
+    localStorage.removeItem($('.song-title').val()); // grab the title and plop here
 
-
+    $(`#${$('.song-title').val()}`).remove();
+    // $('#songs-list').remove(`#${$('.song-title').val()}`);
+    // console.log($('.song-title').val());
+    $('#t1').empty()
+    $(`.${$('.song-title').val()}`).remove(); 
+    $(`#track`).remove(); 
+    $(`.bar`).remove(); 
+    $(".song-title").val("");
+    $(".artist").val("");
+    $(".album").val("");
+    $(".B-P-M").val("");
+    $(".number-of-bars").val("");
+    $(".instruments").val("");
+    $(".song-selector").val("");
   });
 
+  $('.cell').on('click', function(){
+    $('.cell').css('background-color','black');
+  });
 
+}); // end of jquery
 
-   // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let album = $(".user-input").val();
-   //   localStorage.setItem("testStorage", album);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
-
-   $(".del-data-btn").on("click", function() {
-     alert('item deleted? check the console'); // maybe change to a window.confirm
-     localStorage.removeItem( $('.song-title').val() ); // grab the title and plop here
-    //  ('.stored-songs').removeItem($('.song-title').val()); // grab the title and plop here
-     $(".artist").val("");
-     $(".album").val("");
-     // clearing display? what if I have multiple items?
-     // after item is removed from local storage, redisplay items from local storage
-     // refresh from storage?
-   });
-
-
-   // iterative approach to adding items
-   // store data as stringified array of objects
-   // store data with individual keys
-  // how do we get keys? research Object.keys
-
-
-
-});
---
+++ helperFuncs.js
@@ -0,0 +1,34 @@
+let tableGenerator = function(num, array) {
+  $('#t1').empty();
+  
+  if (num === 0) {
+    alert('You need bars once in a while to nurture the soul, so do songs... Please enter the NUMBER of bars.')
+  } else {
+    for (let i = -1; i <= num; i++) {
+      if (i === -1) {
+        $('#t1').append(`<th class="bar1">Instruments</th>`); 
+      } else {
+        $('#t1').append(`<th class="bar">${i}</th>`); 
+      }
+    }
+    
+  // track generator
+    if (array[0] === "") {
+      alert('John Cage\'s 4:33 is not accepted. Please input your instruments...');
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        var value1 = Math.floor(Math.random() * Math.floor(255));
+        var value2 = Math.floor(Math.random() * Math.floor(255));
+        var value3 = Math.floor(Math.random() * Math.floor(255));
+        $('#t1').append(`<tr class=${array[i]}-row><td id='track' class=${array[i]}>${array[i]}</td>`);
+        $(`.${array[i]}`).css('background-color', `rgb(${value1}, ${value2}, ${value3})`);
+      }
+    }
+    for (let i = 0; i < array.length; i++) {
+      for (let j = -1; j < num; j++) {
+        $(`.${array[i]}-row`).append(`<th class='cell'></th>`);
+      }
+    }
+  }
+}; 
+
